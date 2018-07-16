9ed072f58ce5606bed4fc464704f3c22745df649
TejshreeLavatre <tj.241094@gmail.com>
2018-07-15 21:12:54 -0700

    Plotting a parabola function using tkinter

--- Basics/Parabola.py
+++ Basics/Parabola.py
@@ -5,8 +5,21 @@ except ImportError: #Python 2
 
 
 def parabola(x):
-    y = x * x
+    y = x * x / 100
     return y
+
+
+def draw_axes(canvas):
+    canvas.update()
+    x_origin = canvas.winfo_width() / 2
+    y_origin = canvas.winfo_height() / 2
+    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    canvas.create_line(0, -y_origin, 0, y_origin, fill="black")
+
+
+def plot(canvas, x, y):
+    canvas.create_line(x, y, x + 1, y + 1, fill="blue")
 
 
 mainWindow = tkinter.Tk()
@@ -16,8 +29,10 @@ mainWindow.geometry("350x350")
 canvas = tkinter.Canvas(mainWindow, width=350, height=350)
 canvas.grid(row=0, column=0)
 
+draw_axes(canvas)
+
 for w in range(-100, 100):
     z = parabola(w)
-    print(z)
+    plot(canvas, w, -z)
 
 mainWindow.mainloop()
