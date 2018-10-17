b54c301860181809a6da9ea5a87e1c23dcc5bb10
TejshreeLavatre <tj.241094@gmail.com>
2018-10-17 16:06:03 -0700

    edits

--- Basics/Factorial Using Recursion.py
+++ Basics/Factorial Using Recursion.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-def factorial(n):
-    # Calculates n! recursively
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-for i in range(10):
-    print(i, factorial(i))
-=======
 def factorial(n):
     # Calculates n! recursively
     if n <= 1:
