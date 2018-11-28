b371468cc382a70f518b3546ed82435353e0709e
Tejshree <31300134+TejshreeLavatre@users.noreply.github.com>
2018-11-28 15:10:00 -0800

    Parentheses validation check

---
+++ Parenthese validation (using counter).py
@@ -0,0 +1,22 @@
+"""To check whether the parentheses are syntactically valid"""
+
+
+def bracket(foo1):
+    count = 0
+    for i in foo1:
+        if i == "(":
+            count += 1
+        else:
+            if count == 0 and i == ")":
+                return print(foo1, " is Invalid")
+            count -= 1
+
+    if count == 0:
+        return print(foo1, " is Valid")
+    else:
+        return print(foo1, " is Invalid")
+
+
+bracket("())(()")
+bracket("(()())")
+bracket("(())(())")
