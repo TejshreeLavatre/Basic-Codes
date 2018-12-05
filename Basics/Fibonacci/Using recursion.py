149190de0326206c0d351b2ad125984a694ba4c2
Tejshree <31300134+TejshreeLavatre@users.noreply.github.com>
2018-12-05 15:11:27 -0800

    Improved the code

--- Basics/Fibonacci/Using recursion.py
+++ Basics/Fibonacci/Using recursion.py
@@ -1,7 +1,8 @@
 def fibonacci(n):
-    if n == 1 or n == 2:
-        return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 print(fibonacci(6))
