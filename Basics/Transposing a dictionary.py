acc8c16224bbd31bd97a0f97b46d7f2109ac9529
Tejshree <tj.241094@gmail.com>
2019-05-15 01:12:33 -0700

    Duplicate

--- Basics/Transposing a dictionary.py
+++
@@ -1,6 +0,0 @@
-# Transposing a dictionary
-
-food = {"milk": "white", "carrots": "orange", "lemon": "yellow", "grapes": "purple"}
-print(food)
-food_transpose = {food[key]: key for key in food}
-print(food_transpose)
